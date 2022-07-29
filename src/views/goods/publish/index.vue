c99b44269649121e1cb5c5e1f756de15cf1a7f54
weida <weida@eptok.com>
2022-07-29 17:43:41 +0800

    feat #13 商品新增

---
+++ src/views/goods/publish/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app-container">
+    <div style="display: flex;justify-content: center">
+      <el-steps :active="step" finish-status="success" :space="300" style="width: 500px" align-center>
+        <el-step title="商品分类信息"/>
+        <el-step title="商品基本信息"/>
+        <el-step title="商品销售信息"/>
+        <el-step title="商品发布"/>
+      </el-steps>
+    </div>
+    <div v-if="step == 0">
+      <el-form ref="form" :model="categoryForm" :rules="rules" label-width="80px">
+        <el-form-item label="类目" prop="categoryCode">
+          <el-input v-model="categoryForm.categoryCode"/>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div v-if="step == 1">
+
+    </div>
+    <div v-if="step == 2">
+
+    </div>
+
+    <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        step: 0,
+        categoryForm: {}
+      }
+    },
+    methods: {
+      next() {
+        if (this.step++ > 2) {
+          this.step = 0;
+        }
+
+      }
+    }
+  }
+</script>
